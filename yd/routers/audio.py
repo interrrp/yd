78a9144179b1ec971ec064a96acc1353b31e9a51
int <watchyourcache@gmail.com>
2023-08-30 20:16:48 +0800

    Handle when a video is unavailable

--- yd/routers/audio.py
+++ yd/routers/audio.py
@@ -1,7 +1,8 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from fastapi.responses import FileResponse
+from pytube.exceptions import VideoUnavailable
 
 from yd.core import download_audio
 from yd.settings import Settings, get_settings
@@ -14,5 +15,13 @@ async def audio(
     video_id: str, settings: Annotated[Settings, Depends(get_settings)]
 ) -> FileResponse:
     out_path = settings.downloads_dir / f"{video_id}.mp3"
-    await download_audio(video_id, out_path)
+
+    try:
+        await download_audio(video_id, out_path)
+    except VideoUnavailable:
+        raise HTTPException(
+            status_code=404,
+            detail="Video is unavailable, are you sure you have the correct video ID?",
+        )
+
     return FileResponse(out_path)
--- yd/routers/video.py
+++ yd/routers/video.py
@@ -1,7 +1,8 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from fastapi.responses import FileResponse
+from pytube.exceptions import VideoUnavailable
 
 from yd.core import download_video
 from yd.settings import Settings, get_settings
@@ -14,5 +15,13 @@ async def video(
     video_id: str, settings: Annotated[Settings, Depends(get_settings)]
 ) -> FileResponse:
     out_path = settings.downloads_dir / f"{video_id}.mp4"
-    await download_video(video_id, out_path)
+
+    try:
+        await download_video(video_id, out_path)
+    except VideoUnavailable:
+        raise HTTPException(
+            status_code=404,
+            detail="Video is unavailable, are you sure you have the correct video ID?",
+        )
+
     return FileResponse(out_path)
