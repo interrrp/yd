a60ee6ad85179285d9527a10948b5fd99a873ef3
int <watchyourcache@gmail.com>
2023-08-30 18:13:50 +0800

    Load settings from .env

--- yd/settings.py
+++ yd/settings.py
@@ -1,6 +1,8 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
     host: str = "localhost"
     port: int = 8080
+
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
