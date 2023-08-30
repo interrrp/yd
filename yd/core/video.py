365d8a6be4161952200f8422d66e06dc66fdab49
int <watchyourcache@gmail.com>
2023-08-30 20:46:13 +0800

    Make mypy happy

--- yd/core/video.py
+++ yd/core/video.py
@@ -1,7 +1,9 @@
+from pathlib import Path
+
 from pytube import YouTube
 
 
-async def download_video(video_id: str, out_path: str) -> None:
+async def download_video(video_id: str, out_path: Path) -> None:
     (
         YouTube(f"https://www.youtube.com/watch?v={video_id}")
         .streams.order_by("resolution")
